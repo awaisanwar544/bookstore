f13a381e65ba2bc0b564c28212e87634f5bcbe51
awaisanwar544 <awaisanwar544@gmail.com>
2022-03-24 23:06:50 +0500

    initial setup

---
+++ src/components/BookCard..jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookCard = () => (
+  <div className="book">
+    <h3>Title</h3>
+    <p>Author</p>
+    <input type="button" value="Remove" />
+  </div>
+);
+
+export default BookCard;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Categories = () => (
+  <div className="categories" />
+);
+
+export default Categories;
