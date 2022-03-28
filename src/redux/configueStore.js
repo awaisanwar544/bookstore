2bf4670b1ae9378ff752e2c4c21fb9198f41b07c
awaisanwar544 <awaisanwar544@gmail.com>
2022-03-29 00:52:02 +0500

    Change rootReducer and bookReducer

--- src/redux/configueStore.js
+++ src/redux/configueStore.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers } from 'redux';
 import bookReducer from './books/books';
 import statusReducer from './categories/categories';
 
-const rootReducer = combineReducers({ bookReducer, statusReducer });
+const rootReducer = combineReducers({ books: bookReducer, status: statusReducer });
 
 const store = createStore(rootReducer);
 
